76333e6c790d74b13fe575411f6b10a4036da9de
Matthew Revell <matthewrevell@Matthews-MacBook-Pro.local>
2017-04-11 19:41:29 +0100

    Use React

--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
-import "./index.css";
-import numeral from "numeral";
-import { getUsers, deleteUser } from "./api/userApi";
+// @flow
+import React from "react";
+import ReactDOM from "react-dom";
 
-const courseValue = numeral(1000).format("$0,0.00");
-console.log(`I would pay ${courseValue} for this awesome course`); // eslint-disable-line no-console
+const App = () => {
+  return (
+    <div>
+      <label htmlFor="name">Hello World</label>
+    </div>
+  );
+};
 
-getUsers().then(result => {
-  let usersBody = "";
-
-  result.forEach(user => {
-    usersBody += `<tr>
-      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-      <td>${user.id}</td>
-      <td>${user.firstName}</td>
-      <td>${user.lastName}</td>
-      <td>${user.email}</td>
-    </tr>`;
-  });
-
-  global.document.getElementById("users").innerHTML = usersBody;
-
-  const deleteLinks = global.document.getElementsByClassName("deleteUser");
-
-  Array.from(deleteLinks, link => {
-    link.onclick = function(event) {
-      const element = event.target;
-      event.preventDefault();
-      deleteUser(element.attributes["data-id"].value);
-      const row = element.parentNode.parentNode;
-      row.parentNode.removeChild(row);
-    };
-  });
-});
+ReactDOM.render(<App />, document.getElementById("app"));
--- src/index.test.js
+++ src/index.test.js
@@ -1,21 +1,22 @@
-import { expect } from "chai";
-import jsDom from "jsdom";
-import fs from "fs";
+// import jsDom from "jsdom";
+// import fs from "fs";
 
 describe("Our first test", () => {
   it("should pass", () => {
-    expect(true).to.equal(true);
+    expect(true).toBe(true);
   });
 });
 
+/* TODO: Move to JEST
 describe("index.html", () => {
   it("should say h1 that says Users", done => {
     const index = fs.readFileSync("./src/index.html", "utf-8");
     jsDom.env(index, function(err, window) {
-      const h1 = window.document.getElementsByTagName("h1")[0];
-      expect(h1.innerHTML).to.equal("Users");
+      const h1 = window.document.getElementsByTagName("label")[0];
+      expect(h1.innerHTML).to.equal("Hello World");
       done();
       window.close();
     });
   });
 });
+*/
