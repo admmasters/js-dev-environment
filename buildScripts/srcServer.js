e2006a05286867a1967b5b1ae78db2b6630e1e1f
Matt Revell <mattrevell82@me.com>
2017-04-14 16:13:10 +0100

    Update the dependencies and scripts

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,7 +5,6 @@ import webpack from "webpack";
 import config from "../webpack.config.dev";
 
 /* eslint-disable no-console */
-
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
@@ -17,37 +16,16 @@ app.use(
   })
 );
 
+app.use(require("webpack-hot-middleware")(compiler));
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
-});
-
-app.get("/users", function(req, res) {
-  res.json([
-    {
-      id: 1,
-      firstName: "Test1",
-      lastName: "Smith 1",
-      email: "john.smith@example.com"
-    },
-    {
-      id: 2,
-      firstName: "Test2",
-      lastName: "Smith 2",
-      email: "john.smith2@example.com"
-    },
-    {
-      id: 3,
-      firstName: "Test3",
-      lastName: "Smith 3",
-      email: "john.smith3@example.com"
-    }
-  ]);
 });
 
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    open("http://localhost:" + port);
+    open(`http://localhost:${port}`);
   }
 });
