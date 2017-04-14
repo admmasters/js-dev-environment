5494797e4d6d419d13d4787b1325a7dbb2f0815d
Matt Revell <mattrevell82@me.com>
2017-04-14 16:18:34 +0100

    Delete old mocha testing code

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,22 +1,5 @@
-// import jsDom from "jsdom";
-// import fs from "fs";
-
-describe("Our first test", () => {
-  it("should pass", () => {
+describe('Our first test', () => {
+  it('should pass', () => {
     expect(true).toBe(true);
   });
 });
-
-/* TODO: Move to JEST
-describe("index.html", () => {
-  it("should say h1 that says Users", done => {
-    const index = fs.readFileSync("./src/index.html", "utf-8");
-    jsDom.env(index, function(err, window) {
-      const h1 = window.document.getElementsByTagName("label")[0];
-      expect(h1.innerHTML).to.equal("Hello World");
-      done();
-      window.close();
-    });
-  });
-});
-*/
